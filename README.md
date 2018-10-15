e98d0dd27441b3316300d4c1c2d545ad588184d1
WalesExcitedMei <mjy021025@gmail.com>
2018-10-15 19:42:19 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Barinfuck Interpreter
+A interpreter for [brainfuck](https://wikipedia.org/wiki/Brainfuck)
+
+# Files
+1. main.cpp: the main source;
+2. compile.sh: shell to compile;
+3. helloworld.bf: a sample brainfuck source;
+4. helloworld.sh: a shell to run `helloworld.bf`;
---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define TOKENS "><+-.,[]"
+#define CODE_SEGMENT_SIZE 30000
+#define STACK_SEGMENT_SIZE 1000
+#define DATA_SEGMENT_SIZE 30000
+ 
+typedef void (*Callback)(void);
+struct {
+  char cs[CODE_SEGMENT_SIZE];  /* Code Segment        */
+  int ip;                      /* Instruction Pointer */
+  char ss[STACK_SEGMENT_SIZE]; /* Stack Segment       */
+  int sp;                      /* Stack Pointer       */
+  char ds[DATA_SEGMENT_SIZE];  /* Data Segment        */
+  int bp;                      /* Base Pointer        */
+  Callback fn[128];
+} vm;
+inline void vm_forward() { vm.bp = (vm.bp + 1) % DATA_SEGMENT_SIZE; }
+inline void vm_backward() {
+  vm.bp = (vm.bp + DATA_SEGMENT_SIZE - 1) % DATA_SEGMENT_SIZE;
+}
+inline void vm_increment() { vm.ds[vm.bp]++; }
+inline void vm_decrement() { vm.ds[vm.bp]--; }
+inline void vm_input() { vm.ds[vm.bp] = getchar(); }
+inline void vm_output() { putchar(vm.ds[vm.bp]); }
+inline void vm_while_entry() {
+  if (vm.ds[vm.bp]) {
+    vm.ss[vm.sp] = vm.ip - 1;
+    vm.sp++;
+  } else {
+    int c = 1;
+    for (vm.ip++; vm.cs[vm.ip] && c; vm.ip++) {
+      if (vm.cs[vm.ip] == '[') c++;
+      else if (vm.cs[vm.ip] == ']') c--;
+    }
+  }
+}
+inline void vm_while_exit() {
+  if (vm.ds[vm.bp]) {
+    vm.sp--;
+    vm.ip = vm.ss[vm.sp];
+  }
+}
+
+inline void setup() {
+  int c;
+  int i;
+  memset(&vm, 0, sizeof(vm));
+  vm.fn['>'] = vm_forward;
+  vm.fn['<'] = vm_backward;
+  vm.fn['+'] = vm_increment;
+  vm.fn['-'] = vm_decrement;
+  vm.fn['.'] = vm_output;
+  vm.fn[','] = vm_input;
+  vm.fn['['] = vm_while_entry;
+  vm.fn[']'] = vm_while_exit;
+  for (i = 0; (c = getchar()) != EOF;) {
+    if (strchr(TOKENS, c)) {
+      vm.cs[i] = c;
+      i++;
+    }
+  }
+}
+
+inline void run() {
+  while (vm.cs[vm.ip]) {
+    vm.fn[vm.cs[vm.ip]]();
+    vm.ip++;
+  }
+}
+
+int main(int argc, char* argv[]) {
+  if (argc > 1) freopen(argv[1], "r", stdin);
+  setup(); run();
+  return 0;
+}
